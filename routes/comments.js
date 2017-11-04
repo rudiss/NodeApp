bd51b2a066b3ce179ddc447b59abbe73f1c7c401
Rudinei Silva <rudislpp@gmail.com>
2017-11-04 18:02:03 -0200

    delete comments route

--- routes/comments.js
+++ routes/comments.js
@@ -67,6 +67,18 @@ router.put("/:comment_id", function(req, res){
     } else {
       res.redirect("/campgrounds/" + req.params.id);
     }
+  });
+});
+
+// DESTROY ROUTE
+router.delete("/:comment_id", function(req, res){
+  //findByIdAndRemove
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if (err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
   })
 })
 module.exports = router;
